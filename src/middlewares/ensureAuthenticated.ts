ea8956ef82878396e88dd74057f5fe89def444bb
Fernando Saraiva <fpsaraiva@gmail.com>
2021-04-30 20:43:26 -0300

    feat: authentication added to create specification route

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(token, "93e22f2882768bdb0bd9dc473680f8ef") as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if(!user) {
+      throw new Error("User does not exist!");
+    }
+
+    next();
+  } catch (error) {
+    throw new Error("Invalid token!");
+  }
+}
